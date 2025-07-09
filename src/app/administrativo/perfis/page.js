8a69e35e01edc3ad0d768d2cd0c3a1f1f3ad0750
Terceros <diego.terceros@ikhon.com.br>
2025-07-09 16:51:02 -0400

    criacao de componente de tabela para listagem de dados no projeto e aplicada na twla de perfis

--- src/app/administrativo/perfis/page.js
+++ src/app/administrativo/perfis/page.js
@@ -1,7 +1,33 @@
 'use client';
 
+import TabelaCrud from '@/componentes/tabelaCrud';
+
 export default function Perfis() {
+
+    const colunas = [
+        { headerName: 'Nome', field: 'nome_responsavel' },
+        { headerName: 'Matrícula', field: 'matricula' },
+        { headerName: 'Cadastro', field: 'data_cadastro' },
+    ];
+
+    const dados = [
+        { nome_responsavel: 'Diego', matricula: '3215', data_cadastro: 30 },
+        { nome_responsavel: 'Ana', matricula: '4548123', data_cadastro: 25 },
+    ];
+
     return (
-        <div>Perfis</div>
+        <div className='bg-gray-100 min-h-screen'>
+            <h1 className='text-4xl font-bold text-center p-4'>Perfis</h1>
+            <TabelaCrud 
+                colDefs={colunas}
+                rowData={dados}
+                editar={() => {
+                    alert("editar perfil")
+                }}
+                excluir={() => {
+                    alert("excluir perfil")
+                }}
+            />
+        </div>
     )
 }
