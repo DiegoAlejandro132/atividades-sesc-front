e071851a5527be3e19c238a4986214ba40d4caf0
Terceros <diego.terceros@ikhon.com.br>
2025-07-09 21:39:45 -0400

    adicionar tabela na tela de inscricoes

--- src/app/administrativo/inscricoes/page.js
+++ src/app/administrativo/inscricoes/page.js
@@ -1,7 +1,37 @@
 'use client';
 
+import TabelaCrud from '@/componentes/tabelaCrud';
+
 export default function Inscricoes() {
+
+    const colunas = [
+        { headerName: 'Cliente',        field: 'nome_cliente' },
+        { headerName: 'Atividade',      field: 'atividade' },
+        { headerName: 'Descrição',      field: 'descricao_atividade' },
+        { headerName: 'Unidade SESC',   field: 'unidade_sesc' },
+    ];
+
+    const dados = [
+        { nome_cliente: 'Cliente 1', atividade: 'atividade 1', descricao_atividade: "descricao", unidade_sesc: 'Unidade 1' },
+        { nome_cliente: 'Cliente 2', atividade: 'atividade 2', descricao_atividade: "descricao", unidade_sesc: 'Unidade 2' },
+    ];
+
     return (
-        <div>Inscricoes</div>
+        <div className='bg-gray-100 min-h-screen'>
+            <div className='flex items-center justify-between px-4'>
+                <h1 className='text-4xl font-bold text-center p-4'>Inscrições</h1>
+            </div>
+            <TabelaCrud 
+                colDefs={colunas}
+                rowData={dados}
+                editar={() => {
+                    alert("editar perfil")
+                }}
+                excluir={() => {
+                    alert("excluir perfil")
+                }}
+            />
+        </div>
     )
 }
+
